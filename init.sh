10bd8d8fa736ef19c850d41bf81fd240b4e971d4
Danil Kulminskiy <iorsadg@gmail.com>
2018-07-15 18:52:57 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,3 +4,5 @@ sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 #sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
 sudo /etc/init.d/gunicorn restart
+cd web
+gunicorn -c etc/gunicorn.conf hello:app
