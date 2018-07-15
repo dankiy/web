150a408e2f540a5ee63def16a7aa83fe1c029276
Danil Kulminskiy <iorsadg@gmail.com>
2018-07-15 16:12:31 +0300

    Create Hello.py

---
+++ Hello.py
@@ -0,0 +1,8 @@
+def wsgi_application(environ, start_response):
+status = '200 OK'
+headers = [ ('Content-Type', 'text/plain') ]
+body = ''
+for line in environ["QUERY_STRING"].split("&"):
+  body += line + "\n"
+start_response(status, headers )
+return [ body ]
